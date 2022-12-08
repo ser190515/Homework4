50b5e37c79b142aef10b842d525a5af5bfda276b
ser19 <ser190515@gmail.com>
2022-12-08 23:33:14 +0200

    Д.З.2

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
             System.out.println("он совершеннолетний");
         if (men < 18)
             System.out.println("он не достиг совершеннолетия, нужно немного подождать");
+
+        int temperature = 5;
+        if (temperature < 5)
+        System.out.println(" на улице " + temperature + " градусов, нужно надеть шапку ");
+        if (temperature >= 5)
+        System.out.println(" на улице " + temperature + " градусов, можно идти без шапки ");
     }
 }
