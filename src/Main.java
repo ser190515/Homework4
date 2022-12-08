cdecef2dd917ee5fbd6aed1bc41229e665da46bd
ser19 <ser190515@gmail.com>
2022-12-08 23:15:30 +0200

    Д.З.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int men = 18;
+        if (men >= 18)
+            System.out.println("он совершеннолетний");
+        if (men < 18)
+            System.out.println("он не достиг совершеннолетия, нужно немного подождать");
     }
 }
